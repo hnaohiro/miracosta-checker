0954a7214968469b067b12164492349a3c5dbdae
hnaohiro <q@hnaohiro.com>
2017-08-27 00:30:33 +0900

    init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require rails-ujs
+//= require turbolinks
+//= require admin-lte/plugins/jQuery/jquery-2.2.3.min
+//= require admin-lte/bootstrap/js/bootstrap.min
+//= require admin-lte/dist/js/app.min
+//= require_tree .

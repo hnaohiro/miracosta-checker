12d640f4ee5645faf023caf021d855ef8d322abd
hnaohiro <q@hnaohiro.com>
2017-08-28 01:08:15 +0900

    html mail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
   def push(job)
     @job = job
-    from = ENV['MAIL_FROM']
+    from = "ミラコスタ チェッカー <#{ENV['MAIL_FROM']}>"
     to = ENV['MAIL_TO'].split(',')
     mail from: from, to: to, subject: 'ミラコスタ チェッカー'
   end
